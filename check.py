2c1cf0c437ec47a873a879919611a191ad1435b9
YunBeomOh <dbsqja353@naver.com>
2023-02-27 10:05:34 +0900

    23_02_27 Day21

--- check.py
+++ check.py
@@ -1,16 +1,15 @@
 def solution(cards1, cards2, goal): 
-    for i in goal: # i = 'i'/'want'/'to'/'drink'/'water' 
-        if i==cards1[0]: # i want to drink water을 cards1,cards2을 연속시켜 조합해서 만들수있는지 확인해야하기에 cards1과cards2의 첫번째 원소랑 계속 같아야 연속해서 만들수있는거임
-            cards1.pop(0)#cards1.pop(0)으로 제일 앞에있는 원소를 계속 뽑아옴 
-            if len(cards1)==0:
-                continue
-        elif i==cards2[0]:
-            cards2.pop(0)
-            if len(cards2)==0:
-                break
+    answer='yes'
+    index1,index2=0,0
+    for word in goal: # i = 'i'/'want'/'to'/'drink'/'water' 
+        if index1<len(cards1) and word==cards1[index1] :
+            index1+=1
+        elif index2<len(cards2) and word==cards2[index2]:
+            index2+=1
         else:
-            return 'No'
-    return 'Yes'
+            answer='no'
+            break
+    return answer
 
 cards1 = ["i", "drink", "water"] 
 cards2 = ["want", "to"] 
